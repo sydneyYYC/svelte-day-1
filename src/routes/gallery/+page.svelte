10974d52011202b56e23a54d99da9352a252ecba
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-02 14:56:15 -0700

    added card components

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,1 +1,13 @@
+<script>
+  import Card from "../../lib/components/card.svelte";
+  const cardText = {
+    title: 'Placeholder Title',
+    description: 'hohohohohohohahahahahahhahahha so much fun in svelte :) wowie zowie i should play more mario winder but i never have enough time :( because im learning too much'
+  }
+
+</script>
 <h1>This is a gallery</h1>
+
+<Card {...cardText}/>
+
+<Card />
