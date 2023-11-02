cdce4666080a46434d814cb711e62272b5ba577e
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-02 10:18:26 -0700

    added tailwind in branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
   let description = "this is my first svelte project :) yay"
 </script>
 
-<h1>Welcome to SvelteKit Dem0</h1>
+<h1 class="text-3xl text-center mt-4">Welcome to SvelteKit Dem0</h1>
+<section class="font-semibold text-center p-4 mt-4">
 <p>{dailyContent}</p>
 <p>{description}</p>
+</section>
 
-<nav>
+<nav class="bg-slate-200">
   <ul>
     <li></li>
     <a href="/gallery"><li>Gallery</li></a>
