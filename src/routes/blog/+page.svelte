6b7c7d086d46ebb28e0f3fe23dee12f371b0b135
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-06 11:32:26 -0700

    end demo

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,7 +3,7 @@
 
 const pageTitle = "Blog Page";
 const pageDescription = "This is my blog page for my Svelte Project";
-const bgColor = "bg-red-200"
+const bgColor = "bg-red-200";
 </script>
 
 <Header {pageTitle} {pageDescription} {bgColor}/>
