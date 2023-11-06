ea696e356f98e2ebec75b3e69f85604895e5fcbb
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-06 12:13:40 -0700

    added conditionals to blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,6 +4,20 @@
 const pageTitle = "Blog Page";
 const pageDescription = "This is my blog page for my Svelte Project";
 const bgColor = "bg-red-200";
+const name = 'Sydney';
 </script>
 
 <Header {pageTitle} {pageDescription} {bgColor}/>
+
+<!-- render a hello message if the name is ash 
+otherwise say GO AWAY b/c godfrey keeps trying to hack my site-->
+
+{#if name === 'Godfrey'}
+<!-- // sometimes its better to deal with the exception as the if -->
+<p>GO AWAY Godfrey >:[ </p>
+{:else if name === "Sydney"}
+<p>hey {name}, hows the portfolio ;) ?</p>
+{:else} 
+<p>Hello {name}</p>
+{/if}
+
