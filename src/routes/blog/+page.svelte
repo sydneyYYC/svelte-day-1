6dfdc16adeae57bf62bd1585fe66a1500f29aace
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-07 09:35:13 -0700

    added more conditoinals to blog page - practice with conditionals and toggles

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,6 +6,10 @@ const pageDescription = "This is my blog page for my Svelte Project";
 const bgColor = "bg-red-200";
 
 let userLogin = true;
+let user = {
+  name:'Ash',
+  login: true,
+}
 
 function greet() {
   if(userLogin){
@@ -15,17 +19,29 @@ function greet() {
 
 function userLoginToggle() {
   // this changes the login state from true to false with the '!' conditional 
-  return userLogin =!userLogin;
+  return user.login =!user.login;
 };
 </script>
 
 <Header {pageTitle} {pageDescription} {bgColor} flexContent>This is for the Flexbox</Header>
 
-
+{#if user.name === 'Godfrey'}
+<p>Stop trying to hack my site</p>
+{:else if user.name === 'Sydney'}
+<p>You're doing great! :D</p>
+{:else}
+<p>Good morning {user.name}!</p>
+{/if}
 <!-- buttons for alert -->
 <!-- note - your event will still need a function to run  -->
+
+<h2 class="text-3xl m-4">Toggles</h2>
 <button on:click={greet} class="rounded-md bg-red-500 px-4 py-2 mx-4 text-white transition ease-liner duration-150 hover:bg-red-700 active:bg-red-900">Send Alert</button>
 <button on:click={userLoginToggle} class="rounded-md bg-blue-500 px-4 py-2 mx-4 text-white transition ease-liner duration-150 hover:bg-blue-700 active:bg-blue-900">Toggle User Login</button>
 
 <!-- component events and event props-->
-<p class="text-5xl m-4">{userLogin}</p>
+{#if user.login=== true}
+<p> Welcome to the site, {user.name}</p>
+<p class="text-2xl m-4">{user.login}</p>
+{/if}
+
